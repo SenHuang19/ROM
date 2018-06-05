9a1bb948c8d5a513883a94186b87b0911416bc07
Fu <yangyang.fu@pnnl.gov>
2018-06-04 18:32:00 -0700

    Add solvers for linear algerbraic equations in python

---
+++ solver/test_dynlinsolver.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  4 17:34:03 2018
+
+@author: Yangyang Fu
+@email: yangyang.fu@pnnl.gov
+"""
+
+import numpy as np
+import linsolver as lin
+
+A=np.array([[1,2],[1,1]])
+b=np.array([[3],[2]])
+tStart = 0
+tEnd = 1200
+dt = 60
+x0=np.array([[0],[0]])
+
+equation = lin.solveDynamic(A,b,tStart=tStart, tEnd=tEnd, dt=dt, x0=x0)
+
+equation.solve()
+
+
---
+++ solver/test_linsolver.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  4 17:34:03 2018
+
+@author: Yangyang Fu
+@email: yangyang.fu@pnnl.gov
+"""
+
+import numpy as np
+import linsolver as lin
+
+A=np.array([[1,2],[1,1]])
+b=np.array([[3],[2]])
+
+equation = lin.solveLinearEquation(A,b)
+
+equation.solve()
+
+print equation.x
